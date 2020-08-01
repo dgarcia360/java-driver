f1a7edb0ccfb750ff6e4ec0a199fcf62fd5fc1d3
dgarcia360 <dgarcia360@outlook.com>
2020-08-01 16:43:44 +0200

    Added "

--- docs/_utils/doxygen.sh
+++ docs/_utils/doxygen.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-# Clone repo
 cd ..
-output_dir = "docs/_build/dirhtml/${GITHUB_REF##*/}/api"
-echo $output_dir
+output_dir = "docs/_build/api"
+
+if [[ -z "${GITHUB_REF}" ]]; then
+    output_dir = "docs/_build/dirhtml/${GITHUB_REF##*/}/api"
+    echo "HTML_OUTPUT = $output_dir" >> doxyfile
+else
+fi
+
 mkdir -p "$output_dir"
-echo "HTML_OUTPUT = $output_dir" >> doxyfile
+echo "$output_dir"
 doxygen doxyfile
