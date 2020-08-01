dd76d1e65dc8c16d10d401cd625fd411e79ed656
dgarcia360 <dgarcia360@outlook.com>
2020-08-01 16:38:53 +0200

    Added $

--- docs/_utils/doxygen.sh
+++ docs/_utils/doxygen.sh
@@ -3,7 +3,7 @@
 # Clone repo
 cd ..
 output_dir = "docs/_build/dirhtml/${GITHUB_REF##*/}/api"
-echo output_dir
-mkdir -p $output_dir
+echo $output_dir
+mkdir -p "$output_dir"
 echo "HTML_OUTPUT = $output_dir" >> doxyfile
 doxygen doxyfile
