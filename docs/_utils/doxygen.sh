27debf6f583d681b6c92e622e08f376c5d1a66fc
dgarcia360 <dgarcia360@outlook.com>
2020-08-01 16:47:01 +0200

    majus

--- docs/_utils/doxygen.sh
+++ docs/_utils/doxygen.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 cd ..
-output_dir = "docs/_build/api"
+OUTPUT_DIR="docs/_build/api"
 
 if [[ -z "${GITHUB_REF}" ]]; then
-    output_dir = "docs/_build/dirhtml/${GITHUB_REF##*/}/api"
-    echo "HTML_OUTPUT = $output_dir" >> doxyfile
+    OUTPUT_DIR="docs/_build/dirhtml/${GITHUB_REF##*/}/api"
+    echo "HTML_OUTPUT = $OUTPUT_DIR" >> doxyfile
 else
 fi
 
-mkdir -p "$output_dir"
-echo "$output_dir"
+mkdir -p "$OUTPUT_DIR"
+echo "$OUTPUT_DIR"
 doxygen doxyfile
