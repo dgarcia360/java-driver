ead46b49d6a46adb1c01898d1f258ae4c1cebc00
dgarcia360 <dgarcia360@outlook.com>
2020-08-01 16:58:27 +0200

    Added doxygen to preview

--- docs/_utils/doxygen.sh
+++ docs/_utils/doxygen.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
 cd ..
-OUTPUT_DIR="docs/_build/api"
-
-if [[ -z "${GITHUB_REF}" ]]; then
+OUTPUT_DIR="docs/_build/dirhtml/api"
+if [[ -v "${GITHUB_REF}" ]]; then
     OUTPUT_DIR="docs/_build/dirhtml/${GITHUB_REF##*/}/api"
     echo "HTML_OUTPUT = $OUTPUT_DIR" >> doxyfile
-else
 fi
 
 mkdir -p "$OUTPUT_DIR"
