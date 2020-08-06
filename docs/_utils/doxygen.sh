354578129be23a70056e31d1231a281246134e11
dgarcia360 <dgarcia360@outlook.com>
2020-08-06 09:33:33 +0200

    Fixed v

--- docs/_utils/doxygen.sh
+++ docs/_utils/doxygen.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+GITHUB_VERSION= $(echo $GITHUB_REF | cut -d'/' -f 3)
+
 OUTPUT_DIR="docs/_build/dirhtml/api"
-if [[ -v "${GITHUB_VERSION}" ]]; then
-    OUTPUT_DIR="docs/_build/dirhtml/${GITHUB_VERSION}/api"
+if [[ -v "$GITHUB_VERSION" ]]; then
+    OUTPUT_DIR="docs/_build/dirhtml/$GITHUB_VERSION/api"
     echo "HTML_OUTPUT = $OUTPUT_DIR" >> doxyfile
 fi
-echo "$OUTPUT_DIR"
-echo "${GITHUB_VERSION}"
+echo $OUTPUT_DIR
+echo $GITHUB_VERSION
 mkdir -p "$OUTPUT_DIR"
 doxygen doxyfile
