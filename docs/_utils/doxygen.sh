b6bacdd55b09dae4830adf89fc4c08908e26b246
dgarcia360 <dgarcia360@outlook.com>
2020-08-07 10:09:55 +0200

    Update link

--- docs/_utils/doxygen.sh
+++ docs/_utils/doxygen.sh
@@ -7,7 +7,5 @@ if [[ -v GITHUB_VERSION ]]; then
     OUTPUT_DIR="docs/_build/dirhtml/$GITHUB_VERSION/api"
     echo "HTML_OUTPUT = $OUTPUT_DIR" >> doxyfile
 fi
-echo $OUTPUT_DIR
-echo $GITHUB_VERSION
 mkdir -p "$OUTPUT_DIR"
 doxygen doxyfile
