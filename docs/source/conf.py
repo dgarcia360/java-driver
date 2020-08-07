b26778416b4ed96a26570ed62d6cfa5ae31d3472
dgarcia360 <dgarcia360@outlook.com>
2020-08-07 13:35:47 +0200

    Update to latest

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -238,9 +238,9 @@ rst_epilog = """
 
 # -- Options for multiversion --------------------------------------------
 # Whitelist pattern for tags (set to None to ignore all tags)
-smv_tag_whitelist = r'\b(test)\b'
+smv_tag_whitelist = r'None'
 # Whitelist pattern for branches (set to None to ignore all branches)
-smv_branch_whitelist = r"^master$"
+smv_branch_whitelist = r"^latest$"
 
 # Whitelist pattern for remotes (set to None to use local branches only)
 smv_remote_whitelist = r"^origin$"
